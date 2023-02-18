c7045a9178e33e91dabd6d5b4711b208d0e06e2a
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-02-18 12:08:40 -0600

    useEffect

--- src/EfectoPage.jsx
+++ src/EfectoPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import axios from 'axios'
 
 const EfectoPage = () => {
-  const [data, setData] = useState()
+  const [data, setData] = useState([])
 
   const getData = async () => {
     const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
